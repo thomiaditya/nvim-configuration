144bc3d6eca368f20e05327097ee725edb8e9efc
Thomi Aditya <thomiaditya@gmail.com>
2022-02-28 00:38:23 +0700

    Test prettier

--- lua/plugins/config/feline.lua
+++ lua/plugins/config/feline.lua
@@ -1,264 +1,275 @@
-local lsp = require('feline.providers.lsp')
-local vi_mode_utils = require('feline.providers.vi_mode')
+local lsp = require("feline.providers.lsp")
+local vi_mode_utils = require("feline.providers.vi_mode")
 local gps = require("nvim-gps")
 
 local force_inactive = {
-  filetypes = {},
-  buftypes = {},
-  bufnames = {}
+	filetypes = {},
+	buftypes = {},
+	bufnames = {}
 }
 
 local components = {
-  active = {{}, {}, {}},
-  inactive = {{}, {}, {}},
+	active = { {}, {}, {} },
+	inactive = { {}, {}, {} }
 }
 
 local colors = {
-  bg = '#282a36',
-  black = '#282a36',
-  yellow = '#f1fa8c',
-  cyan = '#8be9fd',
-  oceanblue = '#45707a',
-  green = '#50fa7b',
-  orange = '#50fa7b',
-  violet = '#d3869b',
-  magenta = '#c14a4a',
-  white = '#f8f8f2',
-  fg = '#f8f8f2',
-  skyblue = '#8be9fd',
-  red = '#ff5555',
+	bg = "#282a36",
+	black = "#282a36",
+	yellow = "#e0af68",
+	cyan = "#7dcfff",
+	oceanblue = "#45707a",
+	green = "#9ece6a",
+	orange = "#ff9e64",
+	violet = "#d3869b",
+	magenta = "#bb9af7",
+	white = "#f8f8f2",
+	fg = "#c0caf5",
+	skyblue = "#8be9fd",
+	red = "#f7768e"
 }
 
 local vi_mode_colors = {
-  NORMAL = 'green',
-  OP = 'green',
-  INSERT = 'red',
-  VISUAL = 'skyblue',
-  LINES = 'skyblue',
-  BLOCK = 'skyblue',
-  REPLACE = 'violet',
-  ['V-REPLACE'] = 'violet',
-  ENTER = 'cyan',
-  MORE = 'cyan',
-  SELECT = 'orange',
-  COMMAND = 'green',
-  SHELL = 'green',
-  TERM = 'green',
-  NONE = 'yellow'
+	NORMAL = "green",
+	OP = "green",
+	INSERT = "red",
+	VISUAL = "skyblue",
+	LINES = "skyblue",
+	BLOCK = "skyblue",
+	REPLACE = "violet",
+	["V-REPLACE"] = "violet",
+	ENTER = "cyan",
+	MORE = "cyan",
+	SELECT = "orange",
+	COMMAND = "green",
+	SHELL = "green",
+	TERM = "green",
+	NONE = "yellow"
 }
 
 local vi_mode_text = {
-  NORMAL = '<|',
-  OP = '<|',
-  INSERT = '|>',
-  VISUAL = '<>',
-  LINES = '<>',
-  BLOCK = '<>',
-  REPLACE = '<>',
-  ['V-REPLACE'] = '<>',
-  ENTER = '<>',
-  MORE = '<>',
-  SELECT = '<>',
-  COMMAND = '<|',
-  SHELL = '<|',
-  TERM = '<|',
-  NONE = '<>'
+	NORMAL = "<|",
+	OP = "<|",
+	INSERT = "|>",
+	VISUAL = "<>",
+	LINES = "<>",
+	BLOCK = "<>",
+	REPLACE = "<>",
+	["V-REPLACE"] = "<>",
+	ENTER = "<>",
+	MORE = "<>",
+	SELECT = "<>",
+	COMMAND = "<|",
+	SHELL = "<|",
+	TERM = "<|",
+	NONE = "<>"
 }
 
 local buffer_not_empty = function()
-  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-    return true
-  end
-  return false
+	if vim.fn.empty(vim.fn.expand("%:t")) ~= 1 then
+		return true
+	end
+	return false
 end
 
 local checkwidth = function()
-  local squeeze_width  = vim.fn.winwidth(0) / 2
-  if squeeze_width > 40 then
-    return true
-  end
-  return false
+	local squeeze_width = vim.fn.winwidth(0) / 2
+	if squeeze_width > 40 then
+		return true
+	end
+	return false
 end
 
-force_inactive.filetypes = {
-  'NvimTree',
-  'dbui',
-  'packer',
-  'startify',
-  'fugitive',
-  'fugitiveblame',
-	'dashboard'
-}
+force_inactive.filetypes =
+	{
+		"NvimTree",
+		"dbui",
+		"packer",
+		"startify",
+		"fugitive",
+		"fugitiveblame",
+		"dashboard"
+	}
 
-force_inactive.buftypes = {
-  'terminal'
-}
+force_inactive.buftypes = { "terminal" }
 
 -- LEFT
 
 -- vi-mode
 components.active[1][1] = {
-  provider = ' NEOVIM ',
-  hl = function()
-    local val = {}
+	provider = " NEOVIM ",
+	hl = function()
+		local val = {}
 
-    val.bg = vi_mode_utils.get_mode_color()
-    val.fg = 'black'
-    val.style = 'bold'
+		val.bg = vi_mode_utils.get_mode_color()
+		val.fg = "black"
+		val.style = "bold"
 
-    return val
-  end,
-  right_sep = ' '
+		return val
+	end,
+	right_sep = " "
 }
 
 -- vi-symbol
 components.active[1][2] = {
-  provider = function()
-    return vi_mode_text[vi_mode_utils.get_vim_mode()]
-  end,
-  hl = function()
-    local val = {}
-    val.fg = vi_mode_utils.get_mode_color()
-    val.bg = 'bg'
-    val.style = 'bold'
-    return val
-  end,
-  right_sep = ' '
+	provider = function()
+		return vi_mode_text[vi_mode_utils.get_vim_mode()]
+	end,
+	hl = function()
+		local val = {}
+		val.fg = vi_mode_utils.get_mode_color()
+		val.bg = "bg"
+		val.style = "bold"
+		return val
+	end,
+	right_sep = " "
 }
 
--- filename
-components.active[1][3] = {
---  provider = function()
---    return vim.fn.expand("%:F")
---  end,
-	provider = {
-		name = 'file_info',
-		opts = {
-			type = 'unique',
-			colored_icon = false,
-			file_modified_icon = '',
-			file_readonly_icon  = '',
-		},
-	},
-  hl = {
-    fg = 'white',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = {
-    str = ' -> ',
-    hl = {
-      fg = 'white',
-      bg = 'bg',
-      style = 'bold'
-    },
-  }
-}
+-- -- filename
+-- components.active[1][3] = {
+--   --  provider = function()
+--   --    return vim.fn.expand("%:F")
+--   --  end,
+--   provider = {
+--     name = "file_info",
+--     opts = {
+--       type = "unique",
+--       colored_icon = false,
+--       file_modified_icon = "",
+--       file_readonly_icon = ""
+--     }
+--   },
+--   hl = {
+--     fg = "white",
+--     bg = "bg",
+--     style = "bold"
+--   },
+--   right_sep = {
+--     str = "",
+--     hl = {
+--       fg = "white",
+--       bg = "bg",
+--       style = "bold"
+--     }
+--   }
+-- }
+--
+-- -- nvimGps
+-- components.active[1][4] = {
+--   provider = function()
+--     return gps.get_location()
+--   end,
+--   enabled = function()
+--     return gps.is_available()
+--   end,
+--   hl = {
+--     fg = "white",
+--     bg = "bg",
+--     style = "bold"
+--   }
+-- }
 
--- nvimGps
-components.active[1][4] = {
-  provider = function() return gps.get_location() end,
-  enabled = function() return gps.is_available() end,
-  hl = {
-    fg = 'white',
-    bg = 'bg',
-    style = 'bold'
-  }
-}
- 
 -- MID
 
 -- gitBranch
-components.active[2][1] = {
-  provider = 'git_branch',
-  hl = {
-    fg = 'yellow',
-    bg = 'bg',
-    style = 'bold'
-  }
+components.active[1][3] = {
+	provider = "git_branch",
+	hl = {
+		fg = "yellow",
+		bg = "bg",
+		style = "bold"
+	}
 }
 
 -- diffAdd
-components.active[2][2] = {
-  provider = 'git_diff_added',
-  hl = {
-    fg = 'green',
-    bg = 'bg',
-    style = 'bold'
-  }
+components.active[2][1] = {
+	provider = "git_diff_added",
+	hl = {
+		fg = "green",
+		bg = "bg",
+		style = "bold"
+	}
 }
 
 -- diffModfified
-components.active[2][3] = {
-  provider = 'git_diff_changed',
-  hl = {
-    fg = 'orange',
-    bg = 'bg',
-    style = 'bold'
-  }
+components.active[2][2] = {
+	provider = "git_diff_changed",
+	hl = {
+		fg = "orange",
+		bg = "bg",
+		style = "bold"
+	}
 }
 
 -- diffRemove
-components.active[2][4] = {
-  provider = 'git_diff_removed',
-  hl = {
-    fg = 'red',
-    bg = 'bg',
-    style = 'bold'
-  },
+components.active[2][3] = {
+	provider = "git_diff_removed",
+	hl = {
+		fg = "red",
+		bg = "bg",
+		style = "bold"
+	}
 }
 
 -- diagnosticErrors
-components.active[2][5] = {
-  provider = 'diagnostic_errors',
-  enabled = function() return lsp.diagnostics_exist(vim.diagnostic.severity.ERROR) end,
-  hl = {
-    fg = 'red',
-    style = 'bold'
-  }
+components.active[2][4] = {
+	provider = "diagnostic_errors",
+	enabled = function()
+		return lsp.diagnostics_exist(vim.diagnostic.severity.ERROR)
+	end,
+	hl = {
+		fg = "red",
+		style = "bold"
+	}
 }
 
 -- diagnosticWarn
-components.active[2][6] = {
-  provider = 'diagnostic_warnings',
-  enabled = function() return lsp.diagnostics_exist(vim.diagnostic.severity.WARN) end,
-  hl = {
-    fg = 'yellow',
-    style = 'bold'
-  }
+components.active[2][5] = {
+	provider = "diagnostic_warnings",
+	enabled = function()
+		return lsp.diagnostics_exist(vim.diagnostic.severity.WARN)
+	end,
+	hl = {
+		fg = "yellow",
+		style = "bold"
+	}
 }
 
 -- diagnosticHint
-components.active[2][7] = {
-  provider = 'diagnostic_hints',
-  enabled = function() return lsp.diagnostics_exist(vim.diagnostic.severity.HINT) end,
-  hl = {
-    fg = 'cyan',
-    style = 'bold'
-  }
+components.active[2][6] = {
+	provider = "diagnostic_hints",
+	enabled = function()
+		return lsp.diagnostics_exist(vim.diagnostic.severity.HINT)
+	end,
+	hl = {
+		fg = "cyan",
+		style = "bold"
+	}
 }
 
 -- diagnosticInfo
-components.active[2][8] = {
-  provider = 'diagnostic_info',
-  enabled = function() return lsp.diagnostics_exist(vim.diagnostic.severity.INFO) end,
-  hl = {
-    fg = 'skyblue',
-    style = 'bold'
-  }
+components.active[2][7] = {
+	provider = "diagnostic_info",
+	enabled = function()
+		return lsp.diagnostics_exist(vim.diagnostic.severity.INFO)
+	end,
+	hl = {
+		fg = "skyblue",
+		style = "bold"
+	}
 }
 
 -- RIGHT
 
 -- LspName
 components.active[3][1] = {
-  provider = 'lsp_client_names',
-  hl = {
-    fg = 'yellow',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = ' '
+	provider = "lsp_client_names",
+	hl = {
+		fg = "grey",
+		bg = "bg",
+		style = "bold"
+	},
+	right_sep = " "
 }
 
 -- fileIcon
@@ -291,41 +302,42 @@ components.active[3][1] = {
 
 -- fileType
 components.active[3][2] = {
-  provider = {
-		name = 'file_type',
-		opts = {
-			filetype_icon = false,
-		},
+	provider = {
+		name = "file_type",
+		opts = { filetype_icon = false }
 	},
-  hl = function()
-    local val = {}
-    local filename = vim.fn.expand('%:t')
-    local extension = vim.fn.expand('%:e')
-    local icon, name  = require'nvim-web-devicons'.get_icon(filename, extension)
-    if icon ~= nil then
-      val.fg = vim.fn.synIDattr(vim.fn.hlID(name), 'fg')
-    else
-      val.fg = 'white'
-    end
-    val.bg = 'bg'
-    val.style = 'bold'
-    return val
-  end,
-  right_sep = ' '
+	hl = function()
+		local val = {}
+		local filename = vim.fn.expand("%:t")
+		local extension = vim.fn.expand("%:e")
+		local icon, name = require"nvim-web-devicons".get_icon(filename, extension)
+		if icon ~= nil then
+			-- val.fg = vim.fn.synIDattr(vim.fn.hlID(name), "fg")
+			val.fg = "grey"
+		else
+			val.fg = "grey"
+		end
+		val.bg = "bg"
+		val.style = "bold"
+		return val
+	end,
+	right_sep = " "
 }
 
 -- fileSize
 components.active[3][3] = {
-  provider = 'file_size',
-  enabled = function() return vim.fn.getfsize(vim.fn.expand('%:t')) > 0 end,
-  hl = {
-    fg = 'skyblue',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = ' '
+	provider = "file_size",
+	enabled = function()
+		return vim.fn.getfsize(vim.fn.expand("%:t")) > 0
+	end,
+	hl = {
+		fg = "grey",
+		bg = "bg",
+		style = "bold"
+	},
+	right_sep = " "
 }
--- 
+--
 -- -- fileFormat
 -- components.active[3][4] = {
 --   provider = function() return '' .. vim.bo.fileformat:upper() .. '' end,
@@ -336,18 +348,18 @@ components.active[3][3] = {
 --   },
 --   right_sep = ' '
 -- }
--- 
+--
 -- fileEncode
 components.active[3][4] = {
-  provider = 'file_encoding',
-  hl = {
-    fg = 'oceanblue',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = ' '
+	provider = "file_encoding",
+	hl = {
+		fg = "grey",
+		bg = "bg",
+		style = "bold"
+	},
+	right_sep = " "
 }
- 
+
 -- rubyVersion
 -- components.active[3][7] = {
 --   provider = function()
@@ -363,26 +375,26 @@ components.active[3][4] = {
 
 -- lineInfo
 components.active[3][5] = {
-  provider = 'position',
-  hl = {
-    fg = 'yellow',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = ' '
+	provider = "position",
+	hl = {
+		fg = "grey",
+		bg = "bg",
+		style = "bold"
+	},
+	right_sep = " "
 }
 
 -- linePercent
 components.active[3][6] = {
-  provider = 'line_percentage',
-  hl = {
-    fg = 'red',
-    bg = 'bg',
-    style = 'bold'
-  },
-  right_sep = ' '
+	provider = "line_percentage",
+	hl = {
+		fg = "grey",
+		bg = "bg",
+		style = "bold"
+	},
+	right_sep = " "
 }
--- 
+--
 -- -- scrollBar
 -- components.active[3][8] = {
 --   provider = 'scroll_bar',
@@ -391,41 +403,38 @@ components.active[3][6] = {
 --     bg = 'bg',
 --   },
 -- }
--- 
+--
 -- INACTIVE
 
 -- fileType
 components.inactive[3][7] = {
-  provider = 'file_type',
-  hl = {
-    fg = 'black',
-    bg = 'cyan',
-    style = 'bold'
-  },
-  left_sep = {
-    str = ' ',
-    hl = {
-      fg = 'NONE',
-      bg = 'cyan'
-    }
-  },
-  right_sep = {
-    {
-      str = ' ',
-      hl = {
-        fg = 'NONE',
-        bg = 'cyan'
-      }
-    },
-    ' '
-  }
+	provider = "file_type",
+	hl = {
+		fg = "black",
+		bg = "cyan",
+		style = "bold"
+	},
+	left_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+			bg = "cyan"
+		}
+	},
+	right_sep = { {
+		str = " ",
+		hl = {
+			fg = "NONE",
+			bg = "cyan"
+		}
+	}, " " }
 }
 
-require('feline').setup({
-  theme = colors,
-  default_bg = bg,
-  default_fg = fg,
-  vi_mode_colors = vi_mode_colors,
-  components = components,
-  force_inactive = force_inactive,
+require("feline").setup({
+	theme = colors,
+	default_bg = bg,
+	default_fg = fg,
+	vi_mode_colors = vi_mode_colors,
+	components = components,
+	force_inactive = force_inactive
 })
--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -1,74 +1,59 @@
 -- Setup nvim-cmp.
-local cmp = require'cmp'
-local lspkind = require'plugins.config.lspkind-nvim'
+local cmp = require"cmp"
+local lspkind = require"plugins.config.lspkind-nvim"
 
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+-- REQUIRED - you must specify a snippet engine -- For `vsnip` users.
+-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users. -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping. -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items. -- For vsnip users.
+-- { name = 'luasnip' }, -- For luasnip users.
+-- { name = 'ultisnips' }, -- For ultisnips users.
+-- { name = 'snippy' }, -- For snippy users.
+	snippet = { expand = function(args)
+		vim.fn["vsnip#anonymous"](args.body)
+	end },
+	mapping = {
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-y>"] = cmp.config.disable,
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close()
+		}),
+		["<CR>"] = cmp.mapping.confirm({ select = true })
+	},
+	sources = cmp.config.sources(
+		{ { name = "nvim_lsp" }, { name = "vsnip" } },
+		{ { name = "buffer" } }
+	)
 })
 
-cmp.setup {
-	formatting = {
-		format = lspkind.cmp_format({
-      	mode = 'symbol', -- show only symbol annotations
-      	maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+cmp.setup{
+	formatting = { format = lspkind.cmp_format({ -- show only symbol annotations -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 
-      	-- The function below will be called before any actual modifications from lspkind
-      	-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      	before = function (entry, vim_item)
-      	  return vim_item
-    	  end
-  	})
-	}
+	-- The function below will be called before any actual modifications from lspkind
+	-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+		mode = "symbol",
+		maxwidth = 50,
+		before = function(entry, vim_item)
+			return vim_item
+		end
+	}) }
 }
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it. 
-  }, {
-    { name = 'buffer' },
-  })
-})
+cmp.setup.filetype("gitcommit", { sources = cmp.config.sources(
+	{ { name = "cmp_git" } }, -- You can specify the `cmp_git` source if you were installed it.
+	{ { name = "buffer" } }
+) })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline("/", {
+	sources = { { name = "buffer" } }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } })
 })
