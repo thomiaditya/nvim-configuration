ed6bc2d1dfd52b70157b441bb4113fd7e0e4b32d
Thomi Aditya <thomiaditya@gmail.com>
2022-03-13 23:53:01 +0700

    Change configuration for nvim tree

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -3,9 +3,7 @@ require'nvim-tree'.setup {
 	view = {
 		side = "left",
 	},
-	open_on_tab = true,
 	ignore_ft_on_setup = {"dashboard"},
-	auto_close = true,
 }
 
 vim.api.nvim_set_keymap("n", "<leader>]", " :NvimTreeToggle<CR>", {noremap = true})
