78aa43635a554a7136f974455ce3bc8ab70c733d
Thomi Aditya <thomiaditya@gmail.com>
2022-04-14 16:54:55 +0700

    Fixing nvim-cmp to be able to select using arrow keys

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -14,6 +14,8 @@ cmp.setup({
 		vim.fn["vsnip#anonymous"](args.body)
 	end },
 	mapping = {
+		["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i" }),
+		["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i" }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
